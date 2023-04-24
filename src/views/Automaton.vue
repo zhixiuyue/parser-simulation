5b11ce329873f9484f2f37ee5150da7d62794f5d
renjunlin <renjunlin@tencent.com>
2023-04-24 20:56:32 +0800

    fix: fix error

--- src/views/Automaton.vue
+++ src/views/Automaton.vue
@@ -1,49 +1,58 @@
 <template>
-    <div class="analysis">
-        <!-- <CustomHeader :step=1 type="LR0" /> -->
-        <div class="argument">
-            <span>增广语法产生式：{{ argument }}</span>
-            <el-dropdown @command="handleCommand">
-                <span class="el-dropdown-link">
-                    {{ selectItems[selectedItem] }}
-                    <el-icon class="el-icon--right">
-                        <arrow-down />
-                    </el-icon>
-                </span>
-                <template #dropdown>
-                    <el-dropdown-menu>
-                        <el-dropdown-item v-for="(item, index) in selectItems" :key="item" :command="index"
-                            :disabled="selectedItem === index">{{
-                                item
-                            }}</el-dropdown-item>
-                    </el-dropdown-menu>
-                </template>
-            </el-dropdown>
-        </div>
-        <D3Graph ref="D3GrapghRef" :graph="graph" :dotIndex="dotIndex"></D3Graph>
-        <div class="control-btn" v-show="selectedItem === 1"><el-button text :icon="ArrowLeft" @click="goBack"
-                :disabled="dotIndex <= 1">上一步</el-button>
-            <el-button text @click="goForward" :disabled="dotIndex >= graph.length">
-                下一步<el-icon class="el-icon--right">
-                    <ArrowRight />
-                </el-icon>
-            </el-button>
-            {{ dotIndex }} / {{ graph.length }}
-        </div>
+  <div class="analysis">
+    <!-- <CustomHeader :step=1 type="LR0" /> -->
+    <div class="argument">
+      <span>增广语法产生式：{{ argument }}</span>
+      <el-dropdown @command="handleCommand">
+        <span class="el-dropdown-link">
+          {{ selectItems[selectedItem] }}
+          <el-icon class="el-icon--right">
+            <arrow-down />
+          </el-icon>
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item
+              v-for="(item, index) in selectItems"
+              :key="item"
+              :command="index"
+              :disabled="selectedItem === index"
+              >{{ item }}</el-dropdown-item
+            >
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
     </div>
-    <!-- <InputString v-if="showDialog" :dialogVisible="showDialog" type="LR0" @saveInput="saveInput" :data="passData"
+    <D3Graph ref="D3GrapghRef" :graph="graph" :dotIndex="dotIndex"></D3Graph>
+    <div class="control-btn" v-show="selectedItem === 1">
+      <el-button
+        text
+        :icon="ArrowLeft"
+        @click="goBack"
+        :disabled="dotIndex <= 1"
+        >上一步</el-button
+      >
+      <el-button text @click="goForward" :disabled="dotIndex >= graph.length">
+        下一步<el-icon class="el-icon--right">
+          <ArrowRight />
+        </el-icon>
+      </el-button>
+      {{ dotIndex }} / {{ graph.length }}
+    </div>
+  </div>
+  <!-- <InputString v-if="showDialog" :dialogVisible="showDialog" type="LR0" @saveInput="saveInput" :data="passData"
             :notShowInput="true" @onClose="onClose" /> -->
 </template>
 
 <script setup>
 // import CustomHeader from '@/components/Header.vue';
-import InputString from '@/components/InputString.vue';
-import { ArrowLeft } from '@element-plus/icons-vue';
-import D3Graph from '@/components/D3Graph.vue';
-import { ref, computed, reactive, watch, onMounted, onUnmounted } from 'vue';
-import { useStore } from 'vuex';
-import { useRouter } from 'vue-router';
-import { LRRoute } from '@/dataList.js';
+import InputString from "@/components/InputString.vue";
+import { ArrowLeft } from "@element-plus/icons-vue";
+import D3Graph from "@/components/D3Graph.vue";
+import { ref, computed, reactive, watch, onMounted, onUnmounted } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+import { LRRoute } from "@/dataList.js";
 
 const store = useStore();
 const router = useRouter();
@@ -59,8 +68,8 @@ const D3GrapghRef = ref(null);
 // }
 
 const startNonTerminal = computed(() => {
-    return store.getters["grammarStore/getStartTNonTer"];
-})
+  return store.getters["grammarStore/getStartTNonTer"];
+});
 
 // const onClose = () => {
 //     if (!startNonTerminal.value) {
@@ -71,7 +80,6 @@ const startNonTerminal = computed(() => {
 // }
 
 const graphArr = ref([]);
-
 // const dfs = (edges, fromNodeId) => {
 //     if (!edges.length) {
 //         return;
@@ -93,154 +101,173 @@ const graphArr = ref([]);
 // }
 
 const stateValue = ref([]);
+const graphSet = ref(new Set());
+const convertEdge = (edgeItem) => {};
 
 const generateDots = (stateNodeValue) => {
-    let newArr = [];
-    if (!stateNodeValue?.length) {
-        return;
+  let newArr = [];
+  if (!stateNodeValue?.length) {
+    return;
+  }
+  stateNodeValue.forEach((item) => {
+    graphSet.value.add(item.id);
+    if (!item?.edges?.length) {
+      return;
     }
-    stateNodeValue.forEach((item) => {
-        if (!item?.edges?.length) {
-            return;
-        }
-        item.edges.forEach((edgeItem) => {
-            graphArr.value = [...graphArr.value,
-            edgeItem?.next?.id === -1 ?
-                `id [label="Accept" shape="none" style="none" ] id${item.id} -> id [ xlabel="${edgeItem?.tocken}"]` :
-                `id${edgeItem?.next?.id} [label="S${edgeItem?.next?.id}\n${edgeItem?.next?.items.join('\n')}"] id${item.id} -> id${edgeItem?.next?.id} [ xlabel="${edgeItem?.tocken}"]`];
-            if (edgeItem?.next) {
-                newArr = [...newArr, edgeItem?.next];
-            }
-        })
-    })
-    stateValue.value = newArr;
-}
+    item.edges.forEach((edgeItem) => {
+      graphArr.value = [
+        ...graphArr.value,
+        edgeItem?.next?.id === -1
+          ? `id [label="Accept" shape="none" style="none" ] id${item.id} -> id [ xlabel="${edgeItem?.tocken}"]`
+          : `id${edgeItem?.next?.id} [label="S${
+              edgeItem?.next?.id
+            }\n${edgeItem?.next?.items.join("\n")}"] id${item.id} -> id${
+              edgeItem?.next?.id
+            } [ xlabel="${edgeItem?.tocken}"]`,
+      ];
+      if (edgeItem?.next && !graphSet.value.has(edgeItem.next.id)) {
+        newArr.push(edgeItem.next);
+      }
+    });
+  });
+  stateValue.value = newArr;
+};
 
 const nonTerminals = computed(() => {
-    return store.getters["grammarStore/getNonTerminal"];
+  return store.getters["grammarStore/getNonTerminal"];
 });
 
 const generateData = () => {
-    const lRParser = store.getters["grammarStore/getLRParser"];
-    const grammar = store.getters["grammarStore/getGrammar"];
-    const terminal = store.getters["grammarStore/getTerminal"];
-    lRParser.generateState(grammar, startNonTerminal.value, nonTerminals.value, terminal);
-    const stateNodeValue = lRParser.stateGraph;
-    store.commit("grammarStore/updateArgument", stateNodeValue?.items[0]);
-    console.log(stateNodeValue);
-    graphArr.value = [`id${stateNodeValue?.id} [label="S${stateNodeValue?.id}\n${stateNodeValue?.items.join('\n')}"] `];
-    stateValue.value = [stateNodeValue];
-    let number = 0;
-    while (stateValue.value.length) {
-        if (number >= 5) {
-            break;
-        }
-        generateDots(stateValue.value);
-        number += 1;
-    }
-    store.commit("grammarStore/updateGraph", graphArr.value);
-}
+  const lRParser = store.getters["grammarStore/getLRParser"];
+  const grammar = store.getters["grammarStore/getGrammar"];
+  const terminal = store.getters["grammarStore/getTerminal"];
+  lRParser.generateState(
+    grammar,
+    startNonTerminal.value,
+    nonTerminals.value,
+    terminal
+  );
+  const stateNodeValue = lRParser.stateGraph;
+  store.commit("grammarStore/updateArgument", stateNodeValue?.items[0]);
+  console.log(stateNodeValue);
+  graphArr.value = [
+    `id${stateNodeValue?.id} [label="S${
+      stateNodeValue?.id
+    }\n${stateNodeValue?.items.join("\n")}"] `,
+  ];
+  stateValue.value = [stateNodeValue];
+  while (stateValue.value.length) {
+    generateDots(stateValue.value);
+  }
+  store.commit("grammarStore/updateGraph", graphArr.value);
+};
 
 const graph = computed(() => {
-    return store.getters["grammarStore/getGraph"];
-})
+  return store.getters["grammarStore/getGraph"];
+});
 
 const argument = computed(() => {
-    return store.getters["grammarStore/getArgument"];
-})
+  return store.getters["grammarStore/getArgument"];
+});
 
 const dotIndex = ref(1);
 
 const goBack = () => {
-    if (dotIndex.value <= 0) return;
-    dotIndex.value = dotIndex.value - 1;
-    D3GrapghRef.value.setDotIndexForGraph(dotIndex.value);
-    D3GrapghRef.value?.render(selectedItem.value);
-}
+  if (dotIndex.value <= 0) return;
+  dotIndex.value = dotIndex.value - 1;
+  D3GrapghRef.value.setDotIndexForGraph(dotIndex.value);
+  D3GrapghRef.value?.render(selectedItem.value);
+};
 
 const goForward = () => {
-    if (dotIndex.value >= graph.value.length) {
-        return;
-    }
-    dotIndex.value = dotIndex.value + 1;
-    D3GrapghRef.value.setDotIndexForGraph(dotIndex.value);
-    D3GrapghRef.value?.render(selectedItem.value);
-}
+  if (dotIndex.value >= graph.value.length) {
+    return;
+  }
+  dotIndex.value = dotIndex.value + 1;
+  D3GrapghRef.value.setDotIndexForGraph(dotIndex.value);
+  D3GrapghRef.value?.render(selectedItem.value);
+};
 
 onMounted(() => {
-    if (!graph.value?.length) {
-        generateData();
-    } else {
-        D3GrapghRef.value?.render(selectedItem.value);
-    }
-})
+  if (!graph.value?.length) {
+    generateData();
+  } else {
+    D3GrapghRef.value?.render(selectedItem.value);
+  }
+});
 
 const handleCommand = (command) => {
-    selectedItem.value = command;
-}
+  selectedItem.value = command;
+};
 
 const selectItems = ["自动播放", "手动播放", "不播放"];
 const selectedItem = ref(2);
 
-watch(() => startNonTerminal, (newValue, preValue) => {
+watch(
+  () => startNonTerminal,
+  (newValue, preValue) => {
     if (!newValue.value) {
-        router.push(LRRoute[1].route);
-        // if (nonTerminals.value.length === 1) {
-        //     store.commit("grammarStore/updateLRStartNonTerminal", nonTerminals.value[0]);
-        // }
-        // else {
-        //     showDialog.value = true;
-        // }
+      router.push(LRRoute[1].route);
+      // if (nonTerminals.value.length === 1) {
+      //     store.commit("grammarStore/updateLRStartNonTerminal", nonTerminals.value[0]);
+      // }
+      // else {
+      //     showDialog.value = true;
+      // }
     } else {
-        if (preValue == undefined) {
-            setTimeout(() => {
-                D3GrapghRef.value?.render(selectedItem.value);
-            }, 1000);
-        }
+      if (preValue == undefined) {
+        setTimeout(() => {
+          D3GrapghRef.value?.render(selectedItem.value);
+        }, 1000);
+      }
     }
-}, {
+  },
+  {
     immediate: true,
-    deep: true
-})
+    deep: true,
+  }
+);
 
-watch([() => graph.value, selectedItem], ([graphs, item], [preGraph, preItem]) => {
-    if (graphs.length && (item != undefined)) {
-        dotIndex.value = 1;
-        D3GrapghRef.value?.setDotIndexForGraph(dotIndex.value);
-        D3GrapghRef.value?.render(item, graphs);
+watch(
+  [() => graph.value, selectedItem],
+  ([graphs, item], [preGraph, preItem]) => {
+    if (graphs.length && item != undefined) {
+      dotIndex.value = 1;
+      D3GrapghRef.value?.setDotIndexForGraph(dotIndex.value);
+      D3GrapghRef.value?.render(item, graphs);
     }
-},
-    {
-        // immediate: true,
-        deep: true
-    }
+  },
+  {
+    // immediate: true,
+    deep: true,
+  }
 );
 </script>
 
 <style scoped lang="less">
 .analysis {
-    position: relative;
-    height: 100%;
+  position: relative;
+  height: 100%;
 
-    .argument {
-        display: flex;
-        align-items: center;
-        gap: 20px;
-        // justify-content: space-between;
+  .argument {
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    // justify-content: space-between;
 
-        .el-dropdown-link {
-            cursor: pointer;
-            color: var(--el-color-primary);
-            display: flex;
-            align-items: center;
-        }
+    .el-dropdown-link {
+      cursor: pointer;
+      color: var(--el-color-primary);
+      display: flex;
+      align-items: center;
     }
+  }
 
-    .control-btn {
-        position: absolute;
-        bottom: 40px;
-        left: 50%;
-        transform: translate(-50%);
-    }
+  .control-btn {
+    position: absolute;
+    bottom: 40px;
+    left: 50%;
+    transform: translate(-50%);
+  }
 }
 </style>
